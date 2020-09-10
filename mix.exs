99372d4694085a07b64ceac3b0a73eef07a84877
Eugene Yak <geneayak@gmail.com>
2020-09-10 20:03:47 +0300

    enable os_mon

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Bfl.MixProject do
   def application do
     [
       mod: {Bfl.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
