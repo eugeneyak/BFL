858cf4ef1df6b1d405f0db41d2bd43eb41df2cd0
Eugene Yak <geneayak@gmail.com>
2020-09-10 20:29:45 +0300

    real redirect

--- lib/bfl_web/live/page_live.ex
+++ lib/bfl_web/live/page_live.ex
@@ -1,7 +1,7 @@
 defmodule BflWeb.PageLive do
   use BflWeb, :live_view
 
-  alias Bfl.Registry.Lookup
+  alias Bfl.Registry.{Lookup, Collection, Bookmark}
 
   @impl true
   def mount(_params, _session, socket) do
@@ -15,11 +15,19 @@ defmodule BflWeb.PageLive do
 
   @impl true
   def handle_event("commit", _, socket) do
-    IO.inspect("REDIRECT")
-    {:noreply, socket}
+    case socket.assigns.results do
+      [%Collection{bookmarks: bookmarks} | _] ->
+        case bookmarks do
+          [%Bookmark{url: url} | _] -> {:noreply, socket |> redirect(external: url)}
+          _ -> {:noreply, socket}
+        end
+
+      _ ->
+        {:noreply, socket}
+    end
   end
 
-  def cache(user) do
+  defp cache(user) do
     Bfl.Cache.Manager.fetch(user, Bfl.Registry.list_collections())
   end
 end
