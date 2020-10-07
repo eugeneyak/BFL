dab8b139ca04291d89c8383bc42c8680edd60743
Eugene Yak <geneayak@gmail.com>
2020-10-07 23:03:05 +0300

    add deploy

---
+++ Dockerfile
@@ -0,0 +1,49 @@
+FROM elixir:1.9.0-alpine AS build
+
+# install build dependencies
+RUN apk add --no-cache build-base yarn git python
+
+# prepare build dir
+WORKDIR /app
+
+# install hex + rebar
+RUN mix local.hex --force && mix local.rebar --force
+
+# set build ENV
+ENV MIX_ENV=prod
+
+# install mix dependencies
+COPY mix.exs mix.lock ./
+COPY config config
+RUN mix do deps.get, deps.compile
+
+# build assets
+COPY assets/package.json assets/yarn.lock ./assets/
+RUN yarn --cwd ./assets install --immutable
+
+COPY priv priv
+COPY assets assets
+RUN yarn --cwd ./assets deploy
+RUN mix phx.digest
+
+# compile and build release
+COPY lib lib
+# uncomment COPY if rel/ exists
+# COPY rel rel
+RUN mix do compile, release
+
+# prepare release image
+FROM alpine:3.9 AS app
+RUN apk add --no-cache openssl ncurses-libs
+
+WORKDIR /app
+
+RUN chown nobody:nobody /app
+
+USER nobody:nobody
+
+COPY --from=build --chown=nobody:nobody /app/_build/prod/rel/bfl ./
+
+ENV HOME=/app
+
+CMD ["bin/my_app", "start"]
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,38 +4,27 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
-
 config :bfl, Bfl.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+  hostname: System.get_env("DBHOST") || raise("environment variable DBHOST is missing"),
+  database: System.get_env("DBNAME") || "bfl",
+  username: System.get_env("DBUSER") || raise("environment variable DBUSER is missing"),
+  password: System.get_env("DBPASS") || raise("environment variable DBPASS is missing"),
+  port: System.get_env("DBPORT") || 5432,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 3
 
 config :bfl, BflWeb.Endpoint,
-  http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
-  ],
-  secret_key_base: secret_key_base
+  secret_key_base:
+    System.get_env("SECRET_KEY_BASE") ||
+      raise("""
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """)
 
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :bfl, BflWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
+# config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+#   client_id:
+#     System.get_env("UEBERAUTH_GITHUB_CLIENT_ID") ||
+#       raise("environment variable UEBERAUTH_GITHUB_CLIENT_ID is missing"),
+#   client_secret:
+#     System.get_env("UEBERAUTH_GITHUB_CLIENT_SECRET") ||
+#       raise("environment variable UEBERAUTH_GITHUB_CLIENT_SECRET is missing")
